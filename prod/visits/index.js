1e4b716795bedea8a218ac8afb742a63e8dc5791
demonicirfan <hashtag.irfan@gmail.com>
2021-08-30 13:09:12 +0530

    added gitignore docker-compose.yml index.js

--- prod/visits/index.js
+++ prod/visits/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
 const client = redis.createClient({
@@ -9,6 +10,7 @@ const client = redis.createClient({
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
+  process.exit(100);
   client.get("visits", (err, visits) => {
     res.send("Number of visits is " + visits);
     client.set("visits", parseInt(visits) + 1);
@@ -16,5 +18,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8081, () => {
-  console.log("listening on port 8081 🚀");
+  console.log("listening on port 4001:8081 🚀");
 });
